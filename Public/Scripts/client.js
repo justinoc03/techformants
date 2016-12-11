bd1f090980c38287b740f71db7945c978cec1e64
Justin O'Connor <justin.oc03@gmail.com>
2016-12-11 14:24:40 -0600

    fixed angular routing as there was an issue with the angular 1.6 and routing causing a #! and doubling routes

--- Public/Scripts/client.js
+++ Public/Scripts/client.js
@@ -1,1 +1,22 @@
 console.log('JS is sourced, YO');
+
+//source in angular
+var myApp = angular.module("myApp", ['ngRoute']);
+
+///////////////////////////Angular Routing///////////////////////////////////////
+// config method doesnt take a name, we are just configuring myApp,
+// It does take in a dependency injection array
+myApp.config(["$routeProvider", function($routeProvider){
+    $routeProvider.
+      when("/home",{
+        templateUrl: "/views/partials/home.html",
+        controller: "homeController"
+      }).
+      when("/whoWeAre",{
+        templateUrl: "/views/partials/whoWeAre.html",
+        controller: "whoWeAreController"
+      }).
+      otherwise({
+        redirectTo: "/home"
+      });
+}]);
